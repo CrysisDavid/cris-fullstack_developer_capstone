e948b59f24fe3f319f463cd7c7aa265a0436f804
CrysisDavid <davidsoliss123@email.com>
2024-07-29 22:02:08 -0400

    try to fix lint errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,15 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://davidsoliss1-8000." \
+    "https://davidsoliss1-8000."
     "theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
-    "https://davidsoliss1-8000." \
+    "https://davidsoliss1-8000."
     "theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://davidsoliss1-8000." \
+    "https://davidsoliss1-8000."
     "theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
-    "https://davidsoliss1-8000." \
+    "https://davidsoliss1-8000."
     "theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 
@@ -104,17 +104,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation." \
+        "NAME": "django.contrib.auth.password_validation."
                 "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation."
+                "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+                "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+                "NumericPasswordValidator",
     },
 ]
 
